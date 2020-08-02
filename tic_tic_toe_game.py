fdf78f37f9704f987160c7fdef41cf4ca680afa0
Anchusree <anchusreen@gmail.com>
2020-08-02 21:02:32 +0300

    Added butttons for the game

--- tic_tic_toe_game.py
+++ tic_tic_toe_game.py
@@ -3,7 +3,26 @@ from tkinter import *
 tc = Tk()
 tc.title("Tic Tac Toe")
 
-Button1 = Button(tk,text=" ", width=8,height = 4 command = lambda:checker(Button1))
+button1 = Button(tc,text=" ", width=8,height = 4, command = lambda:checker(Button1))
+button2 = Button(tc,text=" ", width=8,height = 4, command = lambda:checker(Button1))
+button3 = Button(tc,text=" ", width=8,height = 4, command = lambda:checker(Button1))
+button4 = Button(tc,text=" ", width=8,height = 4, command = lambda:checker(Button1))
+button5 = Button(tc,text=" ", width=8,height = 4, command = lambda:checker(Button1))
+button6 = Button(tc,text=" ", width=8,height = 4, command = lambda:checker(Button1))
+button7 = Button(tc,text=" ", width=8,height = 4, command = lambda:checker(Button1))
+button8 = Button(tc,text=" ", width=8,height = 4, command = lambda:checker(Button1))
+button9 = Button(tc,text=" ", width=8,height = 4, command = lambda:checker(Button1))
+
+button1.grid(row=1, column = 0)
+button2.grid(row=1, column = 1)
+button3.grid(row=1, column = 2)
+button4.grid(row=2, column = 0)
+button5.grid(row=2, column = 1)
+button6.grid(row=2, column = 2)
+button7.grid(row=3, column = 0)
+button8.grid(row=3, column = 1)
+button9.grid(row=3, column = 2)
+
 
 
 
